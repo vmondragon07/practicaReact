e779d51fd2af3020a769a3ca113b3055ae3742d5
Vianey <vianeykudaipanda07@gmail.com>
2022-10-10 11:29:24 -0500

    Componentes header y listado paciente
    Se crean los componentes header y listado paciente para trabajar mas adelante

---
+++ src/componentes/header.jsx
@@ -0,0 +1,8 @@
+function Header() {
+    //Cada componente debe tener un return
+    //Debe estar en el nivel mas alto y solo 1
+
+    return (<h1>Hola mundo desde react</h1>)
+}
+
+export default Header
